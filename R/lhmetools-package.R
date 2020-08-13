b41cf43f5ed9f325a55ff7dbfd0468533e75ffbd
lhmet <lhmet@ufsm.br>
2020-08-13 18:49:54 -0300

    doc update

---
+++ R/lhmetools-package.R
@@ -0,0 +1,19 @@
+#' lhmetools: convenience functions used in LHMET
+#'
+#'The goal of \pkg{lhmetools} is to to bundle all generic functions frequently
+#'used in LHMET.
+#'
+#'@section lhmetools functions:
+#'So far now the included functions are:
+#' \itemize{
+#'    \item \code{\link{unrar}}
+#' }
+#'
+#'@docType package
+#'@name lhmetools
+#'
+# The following block is used by usethis to automatically manage
+# roxygen namespace tags. Modify with care!
+## usethis namespace: start
+## usethis namespace: end
+"_PACKAGE"
