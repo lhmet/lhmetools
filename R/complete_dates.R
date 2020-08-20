1bfe079c40649ff61cf6d95974c9d89cbaeade9b
lhmet <lhmet@ufsm.br>
2020-08-20 13:16:19 -0300

    first commit

---
+++ R/complete_dates.R
@@ -0,0 +1,72 @@
+#' Complete a data frame with missing combination of variables (date and group)
+#'
+#' @param x data.frame with a column date
+#' @param group character vector of group variables
+#' @param time_step character scalar (e.g., "hours", "days", "months")
+#'
+#' @return tibble with a regular and constant time step
+#' @export
+#'
+#' @examples
+#' # simulated data
+
+#' # usage
+#' datc <- complete_dates(dat, group = c("id", "g"), time_step = "months")
+#' datc
+complete_dates <- function(x, group = "id", time_step = "days") {
+
+  # x = dat; group = c("id", "g"); time_step = "months"
+  assertive::is_subset("date", names(x))
+  assertive::is_subset(group, names(x))
+
+  # TEST DATA
+  # x <- import_qnat(complete = FALSE); group = "id"; time_step = "days"
+  xDT <- data.table::data.table(x, key = c("date", group))
+  rm(x)
+  if (anyDuplicated(xDT) > 0) {
+    message(
+      " There are duplicated dates:",
+      " keeping the first data record."
+    )
+  }
+
+  xDT <- unique(xDT)
+
+  # number of distinct values in the group variable
+  groups_u <- unique(xDT[, ..group])
+  data.table::setkeyv(groups_u[, k := 1], c(data.table::key(groups_u), "k"))
+
+  # time span
+  time_span <- range(xDT[["date"]])
+  # dates with constant and regular time step
+  all_dates <- data.table::data.table(
+    date = seq(
+      from = time_span[1],
+      to = time_span[2],
+      by = time_step
+    ),
+    key = "date"
+  )
+  data.table::setkeyv(all_dates[, k := 1], c(key(all_dates), "k"))
+
+  xDT_c <- data.table::merge.data.table(
+    all_dates,
+    groups_u,
+    by = "k",
+    allow.cartesian = TRUE
+  )[, k := NULL]
+
+  xDT_c <- merge.data.table(
+    xDT_c,
+    xDT,
+    all = TRUE,
+    by = names(xDT_c)
+  )
+
+  data.table::setorderv(
+    x = xDT_c,
+    cols = c(group, "date")
+  )
+
+  return(tibble::as_tibble(xDT_c))
+}
