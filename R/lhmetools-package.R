9f026a4a99be6822660aef9f1b6b4b3c6583c52e
lhmet <lhmet@ufsm.br>
2024-07-24 10:14:39 -0300

    roxygen2 requirement

--- R/lhmetools-package.R
+++ R/lhmetools-package.R
@@ -15,5 +15,7 @@
 # The following block is used by usethis to automatically manage
 # roxygen namespace tags. Modify with care!
 ## usethis namespace: start
+#' @importFrom glue glue
+#' @importFrom lifecycle deprecated
 ## usethis namespace: end
 "_PACKAGE"
